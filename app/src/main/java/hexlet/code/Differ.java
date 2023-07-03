d4be9e8eb2e1872df81c9d1bf7a3dc6d0031e46f
aviloff.rom <aviloff.rom@yandex.ru>
2023-07-03 22:39:04 +0300

    WIP: fix generate() cognitive complexity

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -35,32 +35,32 @@ public class Differ {
         // Объединим все уникальные ключи в одно множество
         var keySet = new HashSet<>(file1.keySet());
         keySet.addAll(file2.keySet());
-
         var keys = new ArrayList<>(keySet);
         Collections.sort(keys);
 
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
         for (String key : keys) {
-            // file1 содержит ключ
-            if (file1.containsKey(key)) {
-                // file2 не содержит ключ
-                if (!file2.containsKey(key)) {
-                    sb.append(TAB).append(PLUS).append(key).append(": ").append(file1.get(key)).append("\n");
-                    continue;
-                }
-                // Значение без изменений
-                if (file1.get(key).equals(file2.get(key))) {
-                    sb.append(TAB).append(TAB).append(key).append(": ").append(file1.get(key)).append("\n");
-                    continue;
-                }
-                // Значение изменено
-                sb.append(TAB).append(MINUS).append(key).append(": ").append(file1.get(key)).append("\n");
-                sb.append(TAB).append(PLUS).append(key).append(": ").append(file2.get(key)).append("\n");
+            if (!file1.containsKey(key)) {
+                // Добавлено
+                sb.append(TAB).append("+ ").append(key).append(": ").append(file2.get(key)).append("\n");
                 continue;
             }
-            // Добавлено
-            sb.append(TAB).append("+ ").append(key).append(": ").append(file2.get(key)).append("\n");
+            // file1 содержит ключ
+
+            // file2 не содержит ключ
+            if (!file2.containsKey(key)) {
+                sb.append(TAB).append(PLUS).append(key).append(": ").append(file1.get(key)).append("\n");
+                continue;
+            }
+            // file2 содержит значение без изменений
+            if (file1.get(key).equals(file2.get(key))) {
+                sb.append(TAB).append(TAB).append(key).append(": ").append(file1.get(key)).append("\n");
+                continue;
+            }
+            // file2 значение изменено
+            sb.append(TAB).append(MINUS).append(key).append(": ").append(file1.get(key)).append("\n");
+            sb.append(TAB).append(PLUS).append(key).append(": ").append(file2.get(key)).append("\n");
         }
         sb.append("}\n");
         return sb.toString();
