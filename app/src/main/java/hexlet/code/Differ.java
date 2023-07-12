cd8b1e211e4d41da538beed1642114d3bb32e321
aviloff.rom <aviloff.rom@yandex.ru>
2023-07-12 13:04:42 +0300

    fixed Differ

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -8,15 +8,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 
 public class Differ {
-    private static final String TAB = "  ";
-    private static final String PLUS = "+ ";
-    private static final String MINUS = "- ";
-
+    public static final String STYLISH = "stylish";
 
     private static Map<String, Object> getData(final String filepath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
@@ -27,39 +25,65 @@ public class Differ {
         });
     }
 
+    public static Map<String, KeyStatus> genDiff(Map<String, Object> file1, Map<String, Object> file2) {
+        Map<String, KeyStatus> result = new HashMap<>();
+        var keySet = new TreeSet<>(file1.keySet());
+        keySet.addAll(file2.keySet());
+        for (String key : keySet) {
+            if (!file1.containsKey(key)) {
+                result.put(key, KeyStatus.ADDED);
+            } else if (!file2.containsKey(key)) {
+                result.put(key, KeyStatus.DELETED);
+            } else if (file1.get(key).equals(file2.get(key))) {
+                result.put(key, KeyStatus.UNCHANGED);
+            } else {
+                result.put(key, KeyStatus.CHANGED);
+            }
+        }
+        return result;
+    }
+
     public static String generate(String format, String filepath1,
                                   String filepath2) throws IOException {
         Map<String, Object> file1 = getData(filepath1);
         Map<String, Object> file2 = getData(filepath2);
 
-        // Объединим все уникальные ключи в одно множество
-        var keySet = new HashSet<>(file1.keySet());
-        keySet.addAll(file2.keySet());
+        var diff = genDiff(file1, file2);
+        return formatDiffResult(format, diff, file1, file2);
+    }
+
+    private static String formatDiffResult(String format,
+                                           Map<String, KeyStatus> diff,
+                                           Map<String, Object> file1,
+                                           Map<String, Object> file2) {
+        switch (format) {
+            case STYLISH -> {
+                return formatStylish(diff, file1, file2);
+            }
+        }
+        return "";
+    }
+
+    private static String formatStylish(
+            Map<String, KeyStatus> diff,
+            Map<String, Object> file1,
+            Map<String, Object> file2) {
+        var keySet = diff.keySet();
         var keys = new ArrayList<>(keySet);
         Collections.sort(keys);
 
         StringBuilder sb = new StringBuilder();
         sb.append("{\n");
         for (String key : keys) {
-            if (!file1.containsKey(key)) {
-                // Добавлено
-                sb.append(TAB).append("+ ").append(key).append(": ").append(file2.get(key)).append("\n");
-                continue;
+            switch (diff.get(key)) {
+                case ADDED -> sb.append("  ").append("+ ").append(key).append(": ").append(file2.get(key)).append("\n");
+                case DELETED -> sb.append("  ").append("- ").append(key).append(": ").append(file1.get(key)).append("\n");
+                case CHANGED -> {
+                    sb.append("  ").append("- ").append(key).append(": ").append(file1.get(key)).append("\n");
+                    sb.append("  ").append("+ ").append(key).append(": ").append(file2.get(key)).append("\n");
+                }
+                case UNCHANGED -> sb.append("  ").append("  ").append(key).append(": ").append(file2.get(key)).append("\n");
             }
-            // file1 содержит ключ
-            // file2 не содержит ключ
-            if (!file2.containsKey(key)) {
-                sb.append(TAB).append(PLUS).append(key).append(": ").append(file1.get(key)).append("\n");
-                continue;
-            }
-            // file2 содержит значение без изменений
-            if (file1.get(key).equals(file2.get(key))) {
-                sb.append(TAB).append(TAB).append(key).append(": ").append(file1.get(key)).append("\n");
-                continue;
-            }
-            // file2 значение изменено
-            sb.append(TAB).append(MINUS).append(key).append(": ").append(file1.get(key)).append("\n");
-            sb.append(TAB).append(PLUS).append(key).append(": ").append(file2.get(key)).append("\n");
         }
         sb.append("}\n");
         return sb.toString();
